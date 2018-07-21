c482d3507c5c69413f5db1203a7f988b8adb88ee
Elbek Djuraev <djuraev.elbek@gmail.com>
2018-07-21 15:20:58 +0300

    simple items

---
+++ app/src/main/java/com/yandex/dev/fixme/views/ItemFactory.kt
@@ -0,0 +1,19 @@
+package com.yandex.dev.fixme.views
+
+import android.widget.ImageView
+import com.yandex.dev.fixme.base.BaseItem
+
+/**
+ * Created by Elbek D. on 21.07.2018.
+ */
+class ItemFactory {
+    companion object {
+        fun createBug(view: ImageView): BaseItem {
+            return BugItem(view)
+        }
+
+        fun createKosil(view: ImageView): BaseItem {
+            return KostilItem(view)
+        }
+    }
+}
