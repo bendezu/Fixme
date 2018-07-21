51366597142a70b78c87bca50bce792a776c1508
bendezu <bendezuvlad@gmail.com>
2018-07-21 13:40:37 +0300

    Setup startMenuActivity

--- app/src/main/java/com/yandex/dev/fixme/StartMenuActivity.kt
+++ app/src/main/java/com/yandex/dev/fixme/StartMenuActivity.kt
@@ -1,12 +1,18 @@
 package com.yandex.dev.fixme
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 
 class StartMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_menu)
+    }
+
+    fun play(view: View) {
+        Toast.makeText(this, "PLAY", Toast.LENGTH_SHORT).show()
     }
 }
