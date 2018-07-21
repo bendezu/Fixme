2806e17fe1bcbb8c74e4c5b53043787c3c2db29e
bendezu <bendezuvlad@gmail.com>
2018-07-21 13:21:24 +0300

    Initial commit

---
+++ app/src/main/java/com/yandex/dev/fixme/GameFieldActivity.kt
@@ -0,0 +1,12 @@
+package com.yandex.dev.fixme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameFieldActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_field)
+    }
+}
---
+++ app/src/main/java/com/yandex/dev/fixme/StartMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.yandex.dev.fixme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_menu)
+    }
+}
