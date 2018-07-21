29e1ea7c668c3bf6365336de3059992855093069
Elbek Djuraev <djuraev.elbek@gmail.com>
2018-07-21 18:40:17 +0300

    game field edit

--- app/src/main/java/com/yandex/dev/fixme/GameFieldActivity.kt
+++ app/src/main/java/com/yandex/dev/fixme/GameFieldActivity.kt
@@ -1,12 +1,12 @@
 package com.yandex.dev.fixme
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import com.yandex.dev.fixme.base.BaseItem
 import com.yandex.dev.fixme.controller.Controller
 import com.yandex.dev.fixme.controller.ViewController
@@ -15,6 +15,7 @@ import kotlinx.android.synthetic.main.activity_game_field.*
 class GameFieldActivity : AppCompatActivity(), ViewController {
     private val handler = Handler(Looper.getMainLooper())
     private lateinit var controller: Controller
+    private val hearts = findViewById<LinearLayout>(R.id.hearts)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,20 +38,19 @@ class GameFieldActivity : AppCompatActivity(), ViewController {
 
     override fun updateScore(score: Int) {
         handler.post {
-            bugFixed.text = String.format(getString(R.string.bugFixed), score)
+            bugFixed.text = getString(R.string.bugFixed).replace("0", score.toString())
         }
     }
 
     override fun updateLife(life: Int) {
         handler.post {
-            hearts.getChildAt(life).visibility = View.INVISIBLE
+            hearts.getChildAt(hearts.childCount - life - 1).visibility = View.INVISIBLE
         }
     }
 
-    override fun exit() {
+    override fun exit(currentScore: Int) {
         handler.post {
-            startActivity(Intent(this, StartMenuActivity::class.java))
-            finish()
+            finalScore.text = "${getString(R.string.finalScore)} $currentScore"
         }
     }
 }
